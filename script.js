d87f6c040257a9a5931c0ca91712d7f11043510c
Kannu Priya <44132532+Kannupriyasingh@users.noreply.github.com>
2024-02-20 23:11:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// function displayMessage(message) {
+//   var messageElement = document.getElementById('message');
+//   messageElement.textContent = message;
+//   messageElement.style.display = 'block'; // Show the message
+
+//   setTimeout(function() {
+//     messageElement.style.display = 'none'; // Hide the message after 3 seconds
+//   }, 3000);
+// }
+
 function displayMessage(message) {
   var messageElement = document.getElementById('message');
   messageElement.textContent = message;
@@ -6,4 +16,14 @@ function displayMessage(message) {
   setTimeout(function() {
     messageElement.style.display = 'none'; // Hide the message after 3 seconds
   }, 3000);
+  
+  // Reset button size after 3 seconds in mobile mode
+  if (window.innerWidth <= 767) {
+    setTimeout(function() {
+      var buttons = document.getElementsByClassName('btn');
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].style.transform = 'scale(1)';
+      }
+    }, 300);
+  }
 }
